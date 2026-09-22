da1d015357f2cc09162d263f52daac2babe1e913
Vishal Jagdish <vishaljagdish85@gmail.com>
2026-09-22 03:23:22 +0200

    included setting env varibale in camera_stream description

--- ros2_ws/src/camera_stream/camera_stream/camera_stream_node.py
+++ ros2_ws/src/camera_stream/camera_stream/camera_stream_node.py
@@ -1,10 +1,12 @@
 #minimal camera feed streamer using libcamera's gstreamer plugin
 #This node streams live camera feed over TCP to a remote device by constructing a GStreamer pipeline using the command line
 
-#TODO; implement h264 encoding, create an RTP payload and stream over UDP
+#TODO: implement h264 encoding, create an RTP payload and stream over UDP
 
 
 #TODO: configure separate pipelines from a single source for snapshot and recording capabilties
+
+#running this requires the environment variable GST_PLUGIN_PATH to point to the libcamera GStreamer plugin installation directory.
 
 
 import rclpy
